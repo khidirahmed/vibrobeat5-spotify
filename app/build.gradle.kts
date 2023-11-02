a3e08b5f62c689965ae26b8fb9a6a7cd1fcd6096
khidirahmed <kka6822@nyu.edu>
2023-11-02 09:52:49 -0400

    Need to revert

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,8 @@ android {
 }
 
 dependencies {
-
+    implementation ("com.spotify.android:spotify-app-remote:2.3.0") // Use the correct version for Spotify App Remote
+    implementation ("com.google.code.gson:gson:2.6.1") // Use a more recent Gson version
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.compose.ui:ui-graphics-android:1.5.4")
--- app/src/main/java/com/example/rad/SongResultActivity.java
+++ app/src/main/java/com/example/rad/SongResultActivity.java
@@ -1,10 +1,11 @@
 package com.example.rad;
 
 import android.content.Intent;
-import android.view.View;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
